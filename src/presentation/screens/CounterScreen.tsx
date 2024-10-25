343135b63e2e398a33b5143c96376babb55c7cd7
Nestor <nestorbardel@gmail.com>
2024-10-25 13:21:20 -0300

    49 - terminada

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Platform } from 'react-native';
 
 export const CounterScreen = () => {
 
@@ -14,8 +14,16 @@ export const CounterScreen = () => {
         <Pressable
           onPress={() => setCount(count + 1)}
           onLongPress={()=> setCount(0)}
+          style={({pressed}) => [
+            styles.button,
+            pressed && styles.buttonPressed,
+          ]}
         >
-          <Text>Incrementar</Text>
+          <Text 
+            style={{color: Platform.OS === 'android' ? 'white' : '#4746AB'}}
+          >
+            Incrementar
+          </Text>
         </Pressable>
     </View>
   );
@@ -33,10 +41,13 @@ const styles = StyleSheet.create({
     fontWeight: '300',
   },
   button:{
-    backgroundColor:'#5856D6',
+    backgroundColor: Platform.OS === 'android' ? '#5856D6' : 'white',
     paddingHorizontal:20,
     paddingVertical:10,
     borderRadius:10,
-  }
+  },
+  buttonPressed:{
+    backgroundColor: Platform.OS === 'android' ? '#4746AB' : 'white',
+  },
 });
 
